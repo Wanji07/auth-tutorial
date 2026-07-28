4a59b917d1286e60426e90c17bd8fe96cd18b68b
Wanji <ewilliammiguel@gmail.com>
2026-07-28 20:05:11 +0800

    feat(verifyEmail: add verify email endpoint

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,12 +2,13 @@ import express from "express"
 
 const router = express.Router()
 
-import { login, signup, logout } from "../controllers/auth.controller.js"
+import { login, signup, logout, verifyEmail } from "../controllers/auth.controller.js"
 
 // Routes for Authentication
 router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
 
+router.post("/verify-email", verifyEmail)
 
 export default router

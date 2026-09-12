9c39a891a7791f676f6e1fed7eac5a7d1df27a8c
Wanji <ewilliammiguel@gmail.com>
2026-09-12 15:32:12 +0800

    frontend(EmailVerification): add EmailVerificationPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import FloatingShape from "./components/FloatingShape";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 import { Routes, Route } from 'react-router-dom'
 
 const App = () =>{
@@ -15,6 +16,7 @@ const App = () =>{
           <Route path="/" element={"Home"} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/verify-email" element={<EmailVerificationPage />} />
         </Routes>
       </div>
     </>
